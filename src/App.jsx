f5446b2c1297f4bd58b3bcfe41f97535c21a6d03
tonybnya <nya.tony2010@gmail.com>
2025-04-16 17:31:21 +0100

    chore: install Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="text-3xl font-bold underline">App</div>;
 };
 
 export default App;
