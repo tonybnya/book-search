0440a81d8979b1d692d3669901c85aeb4e51ad04
tonybnya <nya.tony2010@gmail.com>
2025-04-18 19:23:03 +0100

    fix: update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,11 @@
-// import React from "react";
-// import logo from "/book.png";
-//
-// const Navbar = () => {
-//   return (
-//     <nav className="text-white flex justify-center items-center gap-4 p-6">
-//       <img src={logo} className="h-8 w-8" alt="logo image" />
-//       <span className="text-white text-2xl">
-//         <svg
-//           width="4"
-//           height="30"
-//           viewBox="-1 -1 4 30"
-//           fill="none"
-//           id="svg-511912329_162"
-//         >
-//           <line x1="0.517395" x2="0.517395" y2="28" stroke="white"></line>
-//         </svg>
-//       </span>
-//
-//       <h1 className="text-white font-medium">Book Search</h1>
-//     </nav>
-//   );
-// };
-//
-// export default Navbar;
-
 import React from "react";
 import logo from "/book.png";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-center gap-2 sm:gap-4 px-4 py-2 sm:p-6 text-white w-full">
+    <nav className="flex items-center justify-center gap-4 h-20 p-4 text-white w-full">
       <img src={logo} className="h-6 w-6 sm:h-8 sm:w-8" alt="logo image" />
-      <span className="h-5 sm:h-6">
+      <span className="h-6 animate-pulse">
         <svg
           width="4"
           height="100%"
